fdf4d8b6695420a8341d63bf2af831e0a75ab23d
clincha <angus.clinch@gmail.com>
2024-05-23 22:22:13 +0100

    added in validator and cut down the number of fields

--- pkg/proxmox/network.go
+++ pkg/proxmox/network.go
@@ -8,9 +8,7 @@ import (
 	"io"
 	"log/slog"
 	"net/http"
-	"reflect"
 	"slices"
-	"strings"
 	"time"
 )
 
@@ -95,30 +93,6 @@ func (client *Client) GetNetwork(node *Node, networkName string) (Network, error
 		return network, fmt.Errorf("unable to unmarshall JSON, error was: %v", err)
 	}
 
-	// Check to make sure there aren't any fields that are missing in the struct. Warn if there are missing fields.
-	// More information in: https://github.com/clincha-org/proxmox-api/issues/5
-	var networkJSON map[string]map[string]interface{}
-
-	err = json.Unmarshal(body, &networkJSON)
-	if err != nil {
-		slog.Error(fmt.Sprintf("Unable to unmarshall body for missing field check, error was %v", err))
-	}
-
-	networkModelStruct := reflect.ValueOf(&network).Elem()
-	var NetworkStructFields []string
-	for i := 0; i < networkModelStruct.NumField(); i++ {
-		JSONFieldName := networkModelStruct.Type().Field(i).Tag.Get("json") // Get the JSON representation of the field
-		CommaIndex := strings.Index(JSONFieldName, ",")                     // Get the index of the comma (,omitempty)
-		JSONFieldName = JSONFieldName[:CommaIndex]                          // Remove everything after the comma
-		NetworkStructFields = append(NetworkStructFields, JSONFieldName)
-	}
-
-	for name, _ := range networkJSON["data"] {
-		if !slices.Contains(NetworkStructFields, name) {
-			slog.Warn(fmt.Sprintf("field %q returned by Proxmox API but field does not exist in NetworkModel struct. Please report this to the developers. See: https://github.com/clincha-org/proxmox-api/issues/5", name))
-		}
-	}
-
 	network = networkModel.Data
 	network.Interface = networkName
 
@@ -169,6 +143,11 @@ func (client *Client) CreateNetwork(node *Node, networkRequest *NetworkRequest) 
 }
 
 func (client *Client) UpdateNetwork(node *Node, networkRequest *NetworkRequest) (Network, error) {
+	err := validateNetworkRequest(networkRequest)
+	if err != nil {
+		return Network{}, err
+	}
+
 	var url = client.Host + ApiPath + NodesPath + "/" + node.Node + NetworkPath + "/" + networkRequest.Interface
 
 	slog.Info(fmt.Sprintf("networkRequest struct is %+v", networkRequest))
@@ -272,6 +251,24 @@ func (client *Client) ReloadNetwork(node *Node) error {
 
 	//Allow the network daemon time to reload the configuration
 	time.Sleep(1 * time.Second)
+
+	return nil
+}
+
+func validateNetworkRequest(request *NetworkRequest) error {
+	// Ensure the Interface (name) and the Type are set
+	if request.Interface == "" {
+		return errors.New("interface must be set")
+	}
+	if request.Type == "" {
+		return errors.New("type must be set")
+	}
+
+	// Ensure the Type is set to a valid value
+	var validTypes = []string{"bridge", "bond", "eth", "alias", "vlan", "OVSBridge", "OVSBond", "OVSPort", "OVSIntPort"}
+	if !slices.Contains(validTypes, request.Type) {
+		return fmt.Errorf("type must be one of %+v", validTypes)
+	}
 
 	return nil
 }
--- pkg/proxmox/network_test.go
+++ pkg/proxmox/network_test.go
@@ -1,13 +1,11 @@
 package proxmox
 
 import (
-	"slices"
+	"fmt"
 	"testing"
 )
 
-const PrimaryVirtualBridgeName = "vmbr0"
-
-func TestGetNetworks(t *testing.T) {
+func TestGetDefaultInterface(t *testing.T) {
 	client, err := NewClient(DefaultHostURL, TestUsername, TestPassword)
 	if err != nil {
 		t.Fatal(err)
@@ -18,293 +16,18 @@ func TestGetNetworks(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	networks, err := client.GetNetworks(&nodes[0])
+	node := &nodes[0]
+
+	network, err := client.GetNetwork(node, "vmbr0")
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if len(networks) <= 0 {
-		t.Error("No networks returned by GetNetwork")
+	if network.Interface != "vmbr0" {
+		t.Errorf("Incorrect interface returned. Expected vmbr0, got %v", network.Interface)
 	}
 
-	PrimaryVirtualBridgeIndex := slices.IndexFunc(networks, func(network Network) bool {
-		return network.Interface == PrimaryVirtualBridgeName
-	})
-
-	if PrimaryVirtualBridgeIndex == -1 {
-		t.Errorf("Unable to find primary virtual bridge interface called %q", PrimaryVirtualBridgeName)
-	}
-
-	VirtualBridge := networks[PrimaryVirtualBridgeIndex]
-
-	if VirtualBridge.Interface != PrimaryVirtualBridgeName {
-		t.Errorf("Expected first network to be called %q. Got %q instead", PrimaryVirtualBridgeName, VirtualBridge.Interface)
-	}
-
-	if VirtualBridge.Type != "bridge" {
-		t.Errorf("Expected first network to be of type bridge. Got type %q instead", VirtualBridge.Type)
-	}
-}
-
-func TestGetNetwork(t *testing.T) {
-	client, err := NewClient(DefaultHostURL, TestUsername, TestPassword)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	nodes, err := client.GetNodes()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	network, err := client.GetNetwork(&nodes[0], PrimaryVirtualBridgeName)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if network.Interface != PrimaryVirtualBridgeName {
-		t.Errorf("Expected first network to be called %q. Got %q instead", PrimaryVirtualBridgeName, network.Interface)
-	}
-
-	if network.Type != "bridge" {
-		t.Errorf("Expected first network to be of type bridge. Got type %q instead", network.Type)
-	}
-
-	if network.Method != "static" {
-		t.Errorf("Expected first network method to be static. Got %q instead", network.Method)
-	}
-}
-
-func TestCreateDeleteNetworkBridge(t *testing.T) {
-	client, err := NewClient(DefaultHostURL, TestUsername, TestPassword)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	nodes, err := client.GetNodes()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	testInterface := "vmbr99"
-
-	networkRequest := NetworkRequest{
-		Interface: testInterface,
-		Type:      "bridge",
-	}
-
-	network, err := client.CreateNetwork(&nodes[0], &networkRequest)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if network.Interface != testInterface {
-		t.Errorf("Expected network to be called %q. Got %q instead", testInterface, network.Interface)
-	}
-
-	if network.Type != "bridge" {
-		t.Errorf("Expected network to be of type bridge. Got type %q instead", network.Type)
-	}
-
-	if network.Method != "manual" {
-		t.Errorf("Expected network method to be manual. Got %q instead", network.Method)
-	}
-
-	err = client.DeleteNetwork(&nodes[0], network.Interface)
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestCreateUpdateDeleteNetworkBridge(t *testing.T) {
-	client, err := NewClient(DefaultHostURL, TestUsername, TestPassword)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	nodes, err := client.GetNodes()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	testInterface := "vmbr47"
-	testCIDR := "10.0.3.15/14"
-	//testComments := "testComment"
-
-	networkRequest := NetworkRequest{
-		Type:      "bridge",
-		Interface: testInterface,
-	}
-
-	network, err := client.CreateNetwork(&nodes[0], &networkRequest)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if network.Interface != testInterface {
-		t.Errorf("Expected network to be called %q. Got %q instead", testInterface, network.Interface)
-	}
-
-	if network.Type != "bridge" {
-		t.Errorf("Expected network to be of type bridge. Got type %q instead", network.Type)
-	}
-
-	if network.Method != "manual" {
-		t.Errorf("Expected network method to be manual. Got %q instead", network.Method)
-	}
-
-	networkRequest = NetworkRequest{
-		Interface: network.Interface,
-		Type:      network.Type,
-		AutoStart: 1,
-		CIDR:      testCIDR,
-	}
-
-	network, err = client.UpdateNetwork(&nodes[0], &networkRequest)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if network.Autostart != 1 {
-		t.Errorf("expected autostart to be 1 but got %v", network.Autostart)
-	}
-
-	if network.CIDR != testCIDR {
-		t.Errorf("expected CIDR to be %v but got %v", testCIDR, network.CIDR)
-	}
-
-	networkRequest = NetworkRequest{
-		Interface: network.Interface,
-		Type:      network.Type,
-		Comments:  "Hello",
-		MTU:       8000,
-	}
-
-	network, err = client.UpdateNetwork(&nodes[0], &networkRequest)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if network.Autostart != 1 {
-		t.Errorf("expected autostart to be 1 but got %v", network.Autostart)
-	}
-
-	if network.CIDR != testCIDR {
-		t.Errorf("expected CIDR to be %v but got %v", testCIDR, network.CIDR)
-	}
-
-	//if network.comments != testComments {
-	//	t.Errorf("expected comments to be %v but got %v", testComments, network.Cidr)
-	//}
-
-	err = client.DeleteNetwork(&nodes[0], testInterface)
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestBondedNetworkConfiguration(t *testing.T) {
-	client, err := NewClient(DefaultHostURL, TestUsername, TestPassword)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	nodes, err := client.GetNodes()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	request := NetworkRequest{
-		Interface: "eno1",
-		Type:      "eth",
-		AutoStart: 1,
-	}
-
-	_, err = client.CreateNetwork(&nodes[0], &request)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	request.Interface = "eno2"
-	_, err = client.CreateNetwork(&nodes[0], &request)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	Interface := "bond1"
-	Type := "bond"
-	OVSBonds := "eno1,eno2"
-	BondMode := "active-backup"
-
-	request = NetworkRequest{
-		Interface: Interface,
-		Type:      Type,
-		OVSBonds:  OVSBonds,
-		BondMode:  BondMode,
-	}
-	network, err := client.CreateNetwork(&nodes[0], &request)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if network.Interface != Interface {
-		t.Errorf("expected %v for Interface but got %v", Interface, network.Interface)
-	}
-	if network.Type != Type {
-		t.Errorf("expected %v for Type but got %v", Type, network.Type)
-	}
-	if network.OVSBonds != OVSBonds {
-		t.Errorf("expected %v for OVSBonds but got %v", OVSBonds, network.OVSBonds)
-	}
-	if network.BondMode != BondMode {
-		t.Errorf("expected %v for BondMode but got %v", BondMode, network.BondMode)
-	}
-
-	err = client.DeleteNetwork(&nodes[0], "bond1")
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = client.DeleteNetwork(&nodes[0], "eno1")
-	if err != nil {
-		t.Fatal(err)
-	}
-	err = client.DeleteNetwork(&nodes[0], "eno2")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-}
-
-func TestVLANNetworkConfiguration(t *testing.T) {
-	client, err := NewClient(DefaultHostURL, TestUsername, TestPassword)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	nodes, err := client.GetNodes()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	request := NetworkRequest{
-		Interface: "vmbr74",
-		Type:      "bridge",
-		VlanID:    3,
-	}
-
-	network, err := client.CreateNetwork(&nodes[0], &request)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if network.VlanID != 3 {
-		t.Errorf("expected %v for VlanID but got %v", 3, network.VlanID)
-	}
-
-	err = client.DeleteNetwork(&nodes[0], "vmbr74")
-	if err != nil {
-		t.Fatal(err)
-	}
+	fmt.Printf("%+v", network)
 }
 
 // https://github.com/clincha-org/proxmox-api/issues/11
@@ -319,7 +42,7 @@ func TestNetworkUpdateWithGolangZeroValues(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	node := nodes[0]
+	node := &nodes[0]
 
 	request := NetworkRequest{
 		Interface: "vmbr22",
@@ -327,24 +50,25 @@ func TestNetworkUpdateWithGolangZeroValues(t *testing.T) {
 		VlanID:    2,
 	}
 
-	_, err = client.CreateNetwork(&node, &request)
+	_, err = client.CreateNetwork(node, &request)
 	if err != nil {
 		t.Fatal(err)
 	}
+	t.Cleanup(func() {
+		_ = client.DeleteNetwork(node, "vmbr22")
+	})
 
 	request = NetworkRequest{
 		Interface: "vmbr22",
 		Type:      "bridge",
-		VlanID:    0,
 	}
 
-	network, err := client.UpdateNetwork(&node, &request)
+	network, err := client.UpdateNetwork(node, &request)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	if network.VlanID != 0 {
-		t.Fatalf("Expected VLAN ID to be 0, got %v instead", network.VlanID)
+		t.Fatalf("Expected VLAN ID to be 1, got %v instead", network.VlanID)
 	}
-
 }
