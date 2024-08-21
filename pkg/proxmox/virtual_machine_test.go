9fa14e9920b25952214af5541daf305f946cfcc4
clincha <angus.clinch@gmail.com>
2024-08-21 17:22:36 +0100

    tests running

--- pkg/proxmox/virtual_machine_test.go
+++ pkg/proxmox/virtual_machine_test.go
@@ -31,10 +31,48 @@ func TestGetVM(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	_, err = client.GetVM("pve", 102)
+	cdrom := "local:iso/ubuntu-24.04-live-server-amd64.iso"
+	scsi1 := "local-lvm:8"
+	net1 := "model=virtio,bridge=vmbr0,firewall=1"
+	scsiHardware := "virtio-scsi-pci"
+	cores := int64(1)
+	memory := int64(2048)
+
+	request := VirtualMachineRequest{
+		ID:           102,
+		Cdrom:        &cdrom,
+		SCSI1:        &scsi1,
+		Net1:         &net1,
+		SCSIHardware: &scsiHardware,
+		Cores:        &cores,
+		Memory:       &memory,
+	}
+
+	_, err = client.CreateVM("pve", &request, false)
+	t.Cleanup(func() {
+		err := client.DeleteVM("pve", 102)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
 
 	if err != nil {
 		t.Fatal(err)
+	}
+
+	vm, err := client.GetVM("pve", 102)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if vm.Cores != 1 {
+		t.Errorf("Expected 1 core, got %d", vm.Cores)
+	}
+
+	if vm.Memory != 2048 {
+		t.Errorf("Expected 2048 memory, got %d", vm.Memory)
+
 	}
 }
 
@@ -53,7 +91,7 @@ func TestCreateVM(t *testing.T) {
 	memory := int64(2048)
 
 	request := VirtualMachineRequest{
-		ID:           102,
+		ID:           103,
 		Cdrom:        &cdrom,
 		SCSI1:        &scsi1,
 		Net1:         &net1,
@@ -64,7 +102,7 @@ func TestCreateVM(t *testing.T) {
 
 	vm, err := client.CreateVM("pve", &request, false)
 	t.Cleanup(func() {
-		err := client.DeleteVM("pve", 102)
+		err := client.DeleteVM("pve", 103)
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -99,7 +137,7 @@ func TestCreateVMWithStart(t *testing.T) {
 	memory := int64(2048)
 
 	request := VirtualMachineRequest{
-		ID:           102,
+		ID:           104,
 		Cdrom:        &cdrom,
 		SCSI1:        &scsi1,
 		Net1:         &net1,
@@ -109,20 +147,13 @@ func TestCreateVMWithStart(t *testing.T) {
 	}
 
 	_, err = client.CreateVM("pve", &request, true)
+	t.Cleanup(func() {
+		err := client.DeleteVM("pve", 104)
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
 
-	if err != nil {
-		t.Fatal(err)
-	}
-}
-
-func TestDeleteVM(t *testing.T) {
-	DebugLogs()
-	client, err := NewClient(DefaultHostURL, TestUsername, TestPassword)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = client.DeleteVM("pve", 102)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -143,7 +174,7 @@ func TestUpdateVM(t *testing.T) {
 	memory := int64(2048)
 
 	request := VirtualMachineRequest{
-		ID:           102,
+		ID:           104,
 		Cdrom:        &cdrom,
 		SCSI1:        &scsi1,
 		Net1:         &net1,
@@ -154,7 +185,7 @@ func TestUpdateVM(t *testing.T) {
 
 	_, err = client.CreateVM("pve", &request, true)
 	t.Cleanup(func() {
-		err := client.DeleteVM("pve", 102)
+		err := client.DeleteVM("pve", 104)
 		if err != nil {
 			t.Fatal(err)
 		}
